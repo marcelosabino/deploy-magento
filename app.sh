b84a5fbb9fd8af990f423a88502ec8661e3505a4
mozgbrasil <mozgbrasil@gmail.com>
2019-03-06 23:52:48 -0300

    commit - 06/03/2019 23:52:48

--- app.sh
+++ app.sh
@@ -4,7 +4,7 @@
 # See LICENSE.txt for license details.
 
 setVars () {
-echo -e "${ONYELLOW} setVars ${NORMAL}"
+echo -e "${ONYELLOW} setVars ${RESETCOLOR}"
 
 WICH_7ZA=`which 7za`
 WICH_TAR=`which tar`
@@ -72,20 +72,20 @@ echo
 
 function_after () {
 echo
-echo -e "${ONYELLOW}}${NORMAL}"
+echo -e "${ONYELLOW}}${RESETCOLOR}"
 echo
 }
 
 show_vars () {
 
 function_before
-echo -e "${ONYELLOW} show_vars () { ${NORMAL}"
+echo -e "${ONYELLOW} show_vars () { ${RESETCOLOR}"
 
-echo -e "${ONYELLOW} date ${NORMAL}"
+echo -e "${ONYELLOW} date ${RESETCOLOR}"
 
 echo $(date +%Y-%m-%d_%H-%M-%S)
 
-echo -e "${ONYELLOW} pwd ${NORMAL}"
+echo -e "${ONYELLOW} pwd ${RESETCOLOR}"
 
 pwd && ls
 
@@ -93,19 +93,19 @@ pwd && ls
 
 du -hsx ./* | sort -rh | head -10
 
-echo -e "${ONYELLOW} whoami - print effective userid ${NORMAL}"
+echo -e "${ONYELLOW} whoami - print effective userid ${RESETCOLOR}"
 
 whoami
 
-##echo -e "${ONYELLOW} printenv - print all or part of environment ${NORMAL}"
+##echo -e "${ONYELLOW} printenv - print all or part of environment ${RESETCOLOR}"
 
 #printenv
 
-#echo -e "${ONYELLOW} ps - report a snapshot of the current processes ${NORMAL}"
+#echo -e "${ONYELLOW} ps - report a snapshot of the current processes ${RESETCOLOR}"
 
 #ps aux
 
-#echo -e "${ONYELLOW} will list all the commands you could run. ${NORMAL}"
+#echo -e "${ONYELLOW} will list all the commands you could run. ${RESETCOLOR}"
 
 #compgen -A function -abck
 
@@ -116,11 +116,11 @@ function_after
 mysql_select_admin_user () {
 
 function_before
-echo -e "${ONYELLOW} mysql_select_admin_user () { ${NORMAL}"
+echo -e "${ONYELLOW} mysql_select_admin_user () { ${RESETCOLOR}"
 
 MYSQL_SELECT_ADMIN_USER=`mysql -h "${MAGE_DB_HOST}" -P "${MAGE_DB_PORT}" -u "${MAGE_DB_USER}" -p"${MAGE_DB_PASS}" "${MAGE_DB_NAME}" -N -e "SELECT * FROM admin_user"`
 
-echo -e "${ONPURPLE} - ${NORMAL}"
+echo -e "${ONPURPLE} - ${RESETCOLOR}"
 
 #echo $MYSQL_SELECT_ADMIN_USER
 
@@ -131,13 +131,13 @@ function_after
 magento_config_xml () {
 
 function_before
-echo -e "${ONYELLOW} magento_config_xml () { ${NORMAL}"
+echo -e "${ONYELLOW} magento_config_xml () { ${RESETCOLOR}"
 
-echo -e "${ONYELLOW} Check local.xml ${NORMAL}"
+echo -e "${ONYELLOW} Check local.xml ${RESETCOLOR}"
 
 pwd
 
-echo -e "${ONYELLOW} check n98-magerun ${NORMAL}"
+echo -e "${ONYELLOW} check n98-magerun ${RESETCOLOR}"
 
 timeProg=`which n98-magerun`
 
@@ -145,7 +145,7 @@ timeProg=`which n98-magerun`
 [[ -f "./n98-magerun.phar" ]] || { echo "n98-magerun local installed" 1>&2 ; }
 
 if [ ! -f "./n98-magerun.phar" ]; then # -z String, True if string is empty.
-  echo -e "${ONYELLOW} n98-magerun ${NORMAL}"
+  echo -e "${ONYELLOW} n98-magerun ${RESETCOLOR}"
   wget https://files.magerun.net/n98-magerun.phar
   chmod +x ./n98-magerun.phar
 fi
@@ -161,7 +161,7 @@ function_after
 magento_sample_data_import_haifeng () {
 
 function_before
-echo -e "${ONYELLOW} magento_sample_data_import_haifeng () { ${NORMAL}"
+echo -e "${ONYELLOW} magento_sample_data_import_haifeng () { ${RESETCOLOR}"
 
 #grep -ri 'LOCK TABLE' magento/vendor/haifeng-ben-zhang/magento1.9.2.4-sample-data/magento_sample_data_for_1.9.2.4.sql
 
@@ -170,12 +170,12 @@ awk '/LOCK TABLE/{n=1}; n {n--; next}; 1' < magento/vendor/haifeng-ben-zhang/mag
 
 #grep -ri 'LOCK TABLE' magento/vendor/haifeng-ben-zhang/magento1.9.2.4-sample-data/magento_sample_data_for_1.9.2.4_unlock.sql
 
-echo -e "${ONYELLOW} Importando... ${NORMAL}"
+echo -e "${ONYELLOW} Importando... ${RESETCOLOR}"
 
 if [ -f ".env" ] ; then # if file not exits, only local
-    echo -e "${RED} .env ${NORMAL}"
+    echo -e "${RED} .env ${RESETCOLOR}"
     MYSQL_IMPORT=`mysql -h "${MAGE_DB_HOST}" -P "${MAGE_DB_PORT}" -u "${MAGE_DB_USER}" -p"${MAGE_DB_PASS}" "${MAGE_DB_NAME}" < 'magento/vendor/haifeng-ben-zhang/magento1.9.2.4-sample-data/magento_sample_data_for_1.9.2.4_unlock.sql'` # Heroku, Error R10 (Boot timeout) -> Web process failed to bind to $PORT within 60 seconds of launch
-    echo -e "${RED} MYSQL_IMPORT=${MYSQL_IMPORT} ${NORMAL}"
+    echo -e "${RED} MYSQL_IMPORT=${MYSQL_IMPORT} ${RESETCOLOR}"
 fi
 
 #
@@ -189,23 +189,23 @@ function_after
 magento_install () {
 
 function_before
-echo -e "${ONYELLOW} magento_install () { ${NORMAL}"
+echo -e "${ONYELLOW} magento_install () { ${RESETCOLOR}"
 
-echo -e "${ONYELLOW} pwd ${NORMAL}"
+echo -e "${ONYELLOW} pwd ${RESETCOLOR}"
 
 pwd && ls
 
-echo -e "${ONYELLOW} path ${NORMAL}"
+echo -e "${ONYELLOW} path ${RESETCOLOR}"
 
 pwd && ls
 cd magento
 pwd && ls
 
-#echo -e "${ONYELLOW} Aplicando permissões ${NORMAL}"
+#echo -e "${ONYELLOW} Aplicando permissões ${RESETCOLOR}"
 
 #chmod 777 -R .
 
-echo -e "${ONYELLOW} install.php ${NORMAL}"
+echo -e "${ONYELLOW} install.php ${RESETCOLOR}"
 
 php -f install.php -- \
 --license_agreement_accepted "yes" \
@@ -228,61 +228,61 @@ php -f install.php -- \
 --admin_username "admin" \
 --admin_password "123456a"
 
-echo -e "${ONYELLOW} magento/index.php ${NORMAL}"
+echo -e "${ONYELLOW} magento/index.php ${RESETCOLOR}"
 
 php index.php
 
-echo -e "${ONYELLOW} shell ${NORMAL}"
+echo -e "${ONYELLOW} shell ${RESETCOLOR}"
 
-echo -e "${ONYELLOW} compiler.php --state ${NORMAL}"
+echo -e "${ONYELLOW} compiler.php --state ${RESETCOLOR}"
 
 php shell/compiler.php --state
 
-echo -e "${ONYELLOW} log.php --clean ${NORMAL}"
+echo -e "${ONYELLOW} log.php --clean ${RESETCOLOR}"
 
 php shell/log.php --clean
 
-echo -e "${ONYELLOW} indexer.php --status ${NORMAL}"
+echo -e "${ONYELLOW} indexer.php --status ${RESETCOLOR}"
 
 php shell/indexer.php --status
 
-echo -e "${ONYELLOW} indexer.php --info ${NORMAL}"
+echo -e "${ONYELLOW} indexer.php --info ${RESETCOLOR}"
 
 php shell/indexer.php --info
 
-echo -e "${ONYELLOW} indexer.php --reindexall ${NORMAL}"
+echo -e "${ONYELLOW} indexer.php --reindexall ${RESETCOLOR}"
 
 php shell/indexer.php --reindexall
 
-echo -e "${ONYELLOW} mage ${NORMAL}"
+echo -e "${ONYELLOW} mage ${RESETCOLOR}"
 
 chmod +x mage
 
 bash ./mage
 
-echo -e "${ONYELLOW} mage-setup ${NORMAL}"
+echo -e "${ONYELLOW} mage-setup ${RESETCOLOR}"
 
 bash ./mage mage-setup
 
-echo -e "${ONYELLOW} sync ${NORMAL}"
+echo -e "${ONYELLOW} sync ${RESETCOLOR}"
 
 bash ./mage sync
 
-echo -e "${ONYELLOW} list-installed ${NORMAL}"
+echo -e "${ONYELLOW} list-installed ${RESETCOLOR}"
 
 bash ./mage list-installed
 
-echo -e "${ONYELLOW} list-upgrades ${NORMAL}"
+echo -e "${ONYELLOW} list-upgrades ${RESETCOLOR}"
 
 bash ./mage list-upgrades
 
-echo -e "${ONYELLOW} path ${NORMAL}"
+echo -e "${ONYELLOW} path ${RESETCOLOR}"
 
 pwd && ls
 cd ..
 pwd && ls
 
-echo -e "${ONYELLOW} - ${NORMAL}"
+echo -e "${ONYELLOW} - ${RESETCOLOR}"
 
 function_after
 
@@ -291,7 +291,7 @@ function_after
 release () {
 
 function_before
-echo -e "${ONYELLOW} release () { ${NORMAL}"
+echo -e "${ONYELLOW} release () { ${RESETCOLOR}"
 
 
 function_after
@@ -302,36 +302,36 @@ post_update_cmd () { # post-update-cmd: occurs after the update command has been
 # Na heroku o Mysql ainda não foi instalado nesse ponto
 
 function_before
-echo -e "${ONYELLOW} post_update_cmd () { ${NORMAL}"
+echo -e "${ONYELLOW} post_update_cmd () { ${RESETCOLOR}"
 
-echo -e "${ONYELLOW} - ${NORMAL}"
+echo -e "${ONYELLOW} - ${RESETCOLOR}"
 
 pwd && ls
 
-echo -e "${ONYELLOW} - ${NORMAL}"
+echo -e "${ONYELLOW} - ${RESETCOLOR}"
 
 du -hsx ./* | sort -rh | head -10
 du -hsx magento/vendor/* | sort -rh | head -10
 
-echo -e "${ONYELLOW} - ${NORMAL}"
+echo -e "${ONYELLOW} - ${RESETCOLOR}"
 
 if [ -d magento/vendor/haifeng-ben-zhang/magento1.9.2.4-sample-data/media ]; then
-    echo -e "${ONYELLOW} haifeng-ben-zhang/magento1.9.2.4-sample-data ${NORMAL}"
+    echo -e "${ONYELLOW} haifeng-ben-zhang/magento1.9.2.4-sample-data ${RESETCOLOR}"
     cp -fr magento/vendor/haifeng-ben-zhang/magento1.9.2.4-sample-data/media/* magento/media/
     cp -fr magento/vendor/haifeng-ben-zhang/magento1.9.2.4-sample-data/skin/* magento/skin/
 fi
 
 if [ -d magento/vendor/ceckoslab/ceckoslab_quicklogin ]; then
-    echo -e "${ONYELLOW} ceckoslab/ceckoslab_quicklogin ${NORMAL}"
+    echo -e "${ONYELLOW} ceckoslab/ceckoslab_quicklogin ${RESETCOLOR}"
     cp -fr magento/vendor/ceckoslab/ceckoslab_quicklogin/app/* magento/app/
 fi
 
 if [ -d magento/vendor/prasathmani/tinyfilemanager ]; then
-    echo -e "${ONYELLOW} prasathmani/tinyfilemanager ${NORMAL}"
+    echo -e "${ONYELLOW} prasathmani/tinyfilemanager ${RESETCOLOR}"
     cp -fr magento/vendor/prasathmani/tinyfilemanager/ .
 fi
 
-echo -e "${ONYELLOW} FIX: Heroku, Compiled slug size: xM is too large (max is 500M). ${NORMAL}"
+echo -e "${ONYELLOW} FIX: Heroku, Compiled slug size: xM is too large (max is 500M). ${RESETCOLOR}"
 
 rm -fr magento/vendor/haifeng-ben-zhang/magento1.9.2.4-sample-data/media
 rm -fr magento/vendor/haifeng-ben-zhang/magento1.9.2.4-sample-data/skin
@@ -342,17 +342,17 @@ du -hsx ./magento/media/catalog/product/* | sort -rh | head -10
 rm -fr ./magento/media/catalog/product/p
 rm -fr ./magento/media/catalog/product/cache
 
-echo -e "${ONYELLOW} - ${NORMAL}"
+echo -e "${ONYELLOW} - ${RESETCOLOR}"
 
 du -hsx ./* | sort -rh | head -10
 du -hsx magento/vendor/* | sort -rh | head -10
 
-echo -e "${ONYELLOW} - ${NORMAL}"
+echo -e "${ONYELLOW} - ${RESETCOLOR}"
 
 show_vars
 profile
 
-echo -e "${ONYELLOW} - ${NORMAL}"
+echo -e "${ONYELLOW} - ${RESETCOLOR}"
 
 function_after
 
@@ -361,7 +361,7 @@ function_after
 post_install_cmd () { # post-install-cmd: occurs after the install command has been executed with a lock file present.
 
 function_before
-echo -e "${ONYELLOW} post_install_cmd () { ${NORMAL}"
+echo -e "${ONYELLOW} post_install_cmd () { ${RESETCOLOR}"
 
 post_update_cmd
 
@@ -372,7 +372,7 @@ function_after
 postdeploy () { # postdeploy command. Use this to run any one-time setup tasks that make the app, and any databases, ready and useful for testing.
 
 function_before
-echo -e "${ONYELLOW} postdeploy () { ${NORMAL}"
+echo -e "${ONYELLOW} postdeploy () { ${RESETCOLOR}"
 
 post_update_cmd # post-update-cmd: occurs after the update command has been executed, or after the install command has been executed without a lock file present.
 
@@ -383,11 +383,11 @@ function_after
 profile () { # Heroku, During startup, the container starts a bash shell that runs any code in $HOME/.profile before executing the dyno’s command. You can put bash code in this file to manipulate the initial environment, at runtime, for all dyno types in your app.
 
 function_before
-echo -e "${ONYELLOW} profile () { ${NORMAL}"
+echo -e "${ONYELLOW} profile () { ${RESETCOLOR}"
 
 pwd && ls
 
-echo -e "${ONYELLOW} check mysql ${NORMAL}"
+echo -e "${ONYELLOW} check mysql ${RESETCOLOR}"
 
 if type mysql >/dev/null 2>&1; then
 
@@ -416,7 +416,7 @@ function_after
 
 #
 
-echo -e "${ONYELLOW} .env loading in the shell ${NORMAL}"
+echo -e "${ONYELLOW} .env loading in the shell ${RESETCOLOR}"
 
 dotenv () {
   set -a
@@ -426,7 +426,7 @@ dotenv () {
 
 dotenv
 
-echo -e "${ONYELLOW} env MAGE_ ${NORMAL}"
+echo -e "${ONYELLOW} env MAGE_ ${RESETCOLOR}"
 
 env | grep ^MAGE_
 
@@ -437,7 +437,7 @@ METHOD=${1}
 if [ "$METHOD" ]; then
   $METHOD
 else
-  echo -e "${ONRED} abort () { ${NORMAL}"
+  echo -e "${ONRED} abort () { ${RESETCOLOR}"
 fi
 
 # 
